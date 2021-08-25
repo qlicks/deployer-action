ed9bcaf6b597d86a1df9897f0b83a2fbd4dccceb
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-08-25 14:22:55 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
 PHP_VERSION=$1
-pwd
+
 cd /docker-action
+
 echo "creating docker image with alpine version: $PHP_VERSION"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg php_version="$PHP_VERSION" . && docker run -v $PWD:/app/ docker-action
+docker build -t docker-action --build-arg php_version="$PHP_VERSION" . && docker run -v $GITHUB_WORKSPACE:/app/ docker-action
